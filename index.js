b82692ea6a939cef641f1cc0e031959edb65d00f
alan <sliu.modelo@outlook.com>
2017-10-06 11:26:31 +0800

    lesson 10

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 import App   from './modules/App'
 import About from './modules/About'
@@ -10,7 +10,7 @@ import Home  from './modules/Home'
 
 // render(<App/>, document.getElementById('app'))
 render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App} >
       <IndexRoute component={Home}/>
       
