6c3fa01b77bbc02c736a9485931519d7ba353c49
alan.liu <sliu.modelo@outlook.com>
2017-11-07 17:04:16 +0800

    strict eslint

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 // server.js
-var express = require('express')
-var path = require('path')
-var compression = require('compression')
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
 
-var app = express()
-app.use(compression())
+const app = express();
+app.use(compression());
 // serve our static stuff like index.css
 // app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // send all requests to index.html so browserHistory in React Router works
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-var PORT = process.env.PORT || 8080
-app.listen(PORT, function () {
-  console.log('Production Express server running at localhost:' + PORT)
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  // console.log('Production Express server running at localhost:' + PORT);
+});
