66a637f6746c82a8b343a307c8dd2b808e05fe6e
liuliaixue <liuliaixue@qq.com>
2018-01-28 23:24:59 +0800

    ubuntu

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var app = express()
 app.use(compression())
 // serve our static stuff like index.css
 // app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 var PORT = process.env.PORT || 8080
-app.listen(PORT, function () {
+app.listen(PORT, function () {console.log(PORT)
   console.log('Production Express server running at localhost:' + PORT)
 })
---
+++ test/mobx-1.js
@@ -0,0 +1,13 @@
+import { observable, autorun } from 'mobx';
+
+// const value = observable(0);
+// const number = observable(100);
+
+// autorun(() => {
+//   console.log(value.get());
+// });
+
+// value.set(1);
+// value.set(2);
+// number.set(101);
+import fs from 'fs';
